fc84b7b493078ccbe99c5b50945273fd901cff91
Ricard Derheim <>
2022-11-08 11:51:46 +0100

    Fixed PHP binary path

--- config/config.php
+++ config/config.php
@@ -9,5 +9,5 @@ defined('APP_DIR_LOG') || define('APP_DIR_LOG', realpath(__DIR__ . '/../var/log'
 defined('CMS_URL') || define('CMS_URL' , 'https://cms.visitares.com');
 defined('APP_URL') || define('APP_URL' , 'https://app.visitares.com');
 
-defined('PHP_BIN') || define('PHP_BIN' , '/usr/bin/php7.4');
+defined('PHP_BIN') || define('PHP_BIN' , '/usr/bin/php8.1');
 defined('FFMPEG_BIN') || define('FFMPEG_BIN', '/usr/bin/ffmpeg');
